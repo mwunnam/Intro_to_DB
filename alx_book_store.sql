183a369d639b2cf2110be6d18a0921b48451ae9e
mwunnam <mwunnam@gmail.com>
2025-06-26 18:52:23 +0000

    done with task 0

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,3 +1,11 @@
+CREATE DATABASE IF NOT EXISTS alx_book_store;
+USE alx_book_store;
+
+CREATE TABLE Authors (
+  author_id INT PRIMARY KEY,
+  author_name VARCHAR(215)
+);
+
 CREATE TABLE Books (
   book_id INT PRIMARY KEY,
   title VARCHAR(130),
@@ -5,4 +13,27 @@ CREATE TABLE Books (
   price DOUBLE,
   publication_date DATE,
   FOREIGN KEY (author_id) REFERENCE Authors(author_id)
+);
+
+CREATE TABLE Customers (
+  customer_id INT PRIMARY KEY,
+  customer_name VARCHAR(215),
+  email VARCHAR(215),
+  address TEXT
+);
+
+CREATE TABLE Orders (
+  order_id INT,
+  customer_id INT,
+  order_date DATE,
+  FOREIGN KEY (customer_id) REFERENCE Customers(customer_id)
+);
+
+CREATE TABLE Order_Details (
+  orderdetailid INT PRIMARY KEY,
+  order_id INT,
+  book_id INT,
+  quantity DOUBLE,
+  FOREIGN KEY (order_id) REFERENCE Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCE Books(book_id)
 );
