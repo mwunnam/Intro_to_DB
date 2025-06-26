e88fdda45af622f99fb549932dd56c9d7397f88d
mwunnam <mwunnam@gmail.com>
2025-06-26 20:34:38 +0000

    correction

--- task_6.sql
+++ task_6.sql
@@ -3,5 +3,5 @@ USE alx_book_store;
 INSERT INTO customers (customer_id, customer_name, email, address)
   VALUES (1, "Cole Baidoo", "cbaidoo@sandtech.com", "124 Happiness Ave.");
   VALUES (2, "Blessing Malik", "bmalik@sandtech.com", "124 Happiness  Ave.");
-  VALUES (3, "Obed Ehoneah", "eobed@sandtech.com", "123 Happiness Ave.");
-  VALUES (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness Ave.");
+  VALUES (3, "Obed Ehoneah", "eobed@sandtech.com", "123 Happiness  Ave.");
+  VALUES (4, "Nehemial Kamolu", "nkamolu@sandtech.com", "126 Happiness  Ave.");
