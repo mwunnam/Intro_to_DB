587192958391e9bf05a4285a7bbddf4a549967aa
mwunnam <mwunnam@gmail.com>
2025-06-26 18:55:12 +0000

    correction

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -12,7 +12,7 @@ CREATE TABLE Books (
   author_id INT,
   price DOUBLE,
   publication_date DATE,
-  FOREIGN KEY (author_id) REFERENCE Authors(author_id)
+  FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
 CREATE TABLE Customers (
@@ -26,7 +26,7 @@ CREATE TABLE Orders (
   order_id INT,
   customer_id INT,
   order_date DATE,
-  FOREIGN KEY (customer_id) REFERENCE Customers(customer_id)
+  FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
 CREATE TABLE Order_Details (
@@ -34,6 +34,6 @@ CREATE TABLE Order_Details (
   order_id INT,
   book_id INT,
   quantity DOUBLE,
-  FOREIGN KEY (order_id) REFERENCE Orders(order_id),
-  FOREIGN KEY (book_id) REFERENCE Books(book_id)
+  FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+  FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
